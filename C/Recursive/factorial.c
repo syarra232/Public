699dbac51935be08c122d5dae456df7dd3f49c70
Sreenivas Yarra <70565316+syarra232@users.noreply.github.com>
2020-09-05 22:14:20 -0700

    Factorial using recursion

---
+++ C/Recursive/factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int factorial(int num)
+{
+    if(num < 0)
+        return -1;
+    else if(num == 0)
+        return 1;
+    else
+        return num*factorial(num-1); 
+}
+
+int main(void)
+{
+    printf("-2! = ", factorial(-2));
+    printf("0!  = ", factorial(0));
+    printf("1!  = ", factorial(1));
+    printf("10! = ", factorial(10));
+}
