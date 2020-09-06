0ab2ceb3dddff5be41929a8ada36894ac27d9040
Sreenivas Yarra <70565316+syarra232@users.noreply.github.com>
2020-09-05 22:30:02 -0700

    prefix the function name with my_

--- C/Recursive/factorial.c
+++ C/Recursive/factorial.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-int factorial(int num)
+int my_factorial(int num)
 {
     if(num < 0)
         return -1;
     else if(num == 0)
         return 1;
     else
-        return num*factorial(num-1); 
+        return num * my_factorial(num-1); 
 }
 
 int main(void)
